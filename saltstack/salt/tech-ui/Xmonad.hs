21827144ff53c127e1f317e710f931ecfd02739d
Pieter Vercammen <vercapi@SRN001.localdomain>
2020-03-09 19:38:55 +0000

    Added command to lock screen. changed WMName to fix jdev
    jdeveloper doesn't start because of AWT bug. If the WMname is LG3D the
    bug doesn't appear
    
    https://hackage.haskell.org/package/xmonad-contrib-0.16/docs/XMonad-Hooks-SetWMName.html

--- saltstack/salt/tech-ui/Xmonad.hs
+++ saltstack/salt/tech-ui/Xmonad.hs
@@ -1,21 +1,26 @@
 import XMonad
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.SetWMName
 import qualified Data.Map as M
 
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey desktopConfig
     { terminal    = "termite"
     , modMask     = mod4Mask
     , keys        = \c -> myKeys c `M.union` keys defaultConfig c
+    , startupHook = setWMName "LG3D"
     }
   where
     -- keybindings
-    myKeys (XConfig {modMask = modm}) = M.fromList $
-         [ ((modm, xK_r), spawn launcher)]
+    myKeys (XConfig {modMask = mod4Mask}) = M.fromList $
+         [ ((mod4Mask, xK_r), spawn launcher)
+         , ((mod4Mask .|. controlMask, xK_l), spawn lock)]
 
 myBar = "xmobar"
 
 launcher = "rofi -combi-modi drun,ssh -show combi"
+
+lock = "light-locker-command -l"
 
 myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
 
