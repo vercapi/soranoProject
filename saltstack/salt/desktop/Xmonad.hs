44d8242eee3ea24d2b514382c3e22dd43bf8407f
Pieter Vercammen <vercapi@SRN001.localdomain>
2020-05-13 10:09:30 +0200

    Added xmonad layouts to support wide screens

--- saltstack/salt/desktop/Xmonad.hs
+++ saltstack/salt/desktop/Xmonad.hs
@@ -5,6 +5,43 @@ import XMonad.Hooks.SetWMName
 import qualified Data.Map as M
 import XMonad.Hooks.EwmhDesktops
 import Colors -- generated with pywal
+import XMonad.Layout.Grid
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Circle
+import XMonad.Layout.NoBorders (noBorders, smartBorders)
+
+defaultLayouts = smartBorders(
+
+  -- ThreeColMid layout puts the large master window in the center
+  -- of the screen. As configured below, by default it takes of 3/4 of
+  -- the available space. Remaining windows tile to both the left and
+  -- right of the master window. You can resize using "super-h" and
+  -- "super-l".
+    ThreeColMid 1 (3/100) (3/7)
+
+     -- ResizableTall layout has a large master window on the left,
+  -- and remaining windows tile on the right. By default each area
+  -- takes up half the screen, but you can resize using "super-h" and
+  -- "super-l".
+   ||| ResizableTall 1 (3/100) (1/2) []
+
+  -- Mirrored variation of ResizableTall. In this layout, the large
+  -- master window is at the top, and remaining windows tile at the
+  -- bottom of the screen. Can be resized as described above.
+  ||| Mirror (ResizableTall 1 (3/100) (1/2) [])
+
+  -- Full layout makes every window full screen. When you toggle the
+  -- active window, it will bring the active window to the front.
+  ||| noBorders Full
+  -- Circle layout places the master window in the center of the screen.
+  -- Remaining windows appear in a circle around it
+   ||| Circle
+
+  -- Grid layout tries to equally distribute windows in the available
+  -- space, increasing the number of columns and rows as necessary.
+  -- Master window is at top left.
+  ||| Grid)
 
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey desktopConfig
     { terminal        = "termite"
@@ -12,6 +49,7 @@ main = xmonad =<< statusBar myBar myPP toggleStrutsKey desktopConfig
     , keys            = \c -> myKeys c `M.union` keys defaultConfig c
     , startupHook     = myStartupHook
     , handleEventHook = fullscreenEventHook -- Needed for chromium fullscreen to work
+    , layoutHook      = defaultLayouts
     }
   where
     -- keybindings
